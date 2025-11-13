3fc5e9fa5ad957db8c46573ad151c0baf05d054c
HemantRegmi <hregmi2060@gmail.com>
2025-11-13 12:43:39 +0545

    jenkins added

---
+++ jenkins/scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing dependencies..."
+npm install
+echo "Runnning build..."
+echo "Build completed!"
---
+++ jenkins/scripts/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Running tests..."
+node index.js
+echo "Tests completed!"
